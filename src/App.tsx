cce9fc6584d80eeea9be15ccc67bb6a423b8fc36
nbirhanu <nbirhanu80@gmail.com>
2024-07-25 11:24:55 +0300

    change the js file to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { GlobalStyles } from './styles/Globalstyles'
+import Cabins from './pages/Cabins'
+import Settings from './pages/Settings'
+import Dashboard from './pages/Dashboard'
+import Account from './pages/Account'
+import Bookings from './pages/Bookings'
+import NewUsers from './pages/Users'
+import AppLayout from './ui/AppLayout'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 60 * 1000,
+		},
+	},
+})
 
 export default function App() {
 	return (
-		<div>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
 			<GlobalStyles />
-			<div>wenchi</div>
-		</div>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<AppLayout />}>
+						<Route index element={<Navigate replace to="dashboard" />} />
+						<Route path="dashboard" element={<Dashboard />} />
+						<Route path="bookings" element={<Bookings />} />
+						<Route path="cabins" element={<Cabins />} />
+						<Route path="users" element={<NewUsers />} />
+						<Route path="settings" element={<Settings />} />
+						<Route path="account" element={<Account />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	)
 }
---
+++ src/services/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://ibexhtzqarezyrkxzfsz.supabase.co'
+const supabaseKey =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliZXhodHpxYXJlenlya3h6ZnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE1NjUxNDksImV4cCI6MjAzNzE0MTE0OX0.TxttgLu-sTglzGuvcZvKJn_DjecEsefT68H0mEGfeZI'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
